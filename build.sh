b2724f85465f3ce62edaacd30c67e0e559b7aa56
chrislample <clample@testobject.com>
2016-04-07 14:01:09 +0200

    Update to also push to quay.io

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ docker build --build-arg APPIUM_VERSION=$appium_version -t testobject-appium .
 
 docker tag -f testobject-appium testobject/appium:$appium_version
 docker push testobject/appium:$appium_version
+
+docker tag -f testobject-appium quay.io/testobject/appium:$appium_version
+docker push quay.io/testobject/appium:$appium_version
