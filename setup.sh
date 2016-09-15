3dc14456fbee85af09e6ba87f57b4c36d1041568
chrislample <clample@testobject.com>
2016-09-15 15:45:00 +0200

    Support patched chromedriver
    - Include patched chromedriver executable in repository
    - replace Appium's default chromedriver with the patched version
    - place appium version in directory <appium-version>-patched-chromedriver

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "preparing appium version $APPIUM_VERSION"
+echo "preparing appium version $APPIUM_VERSION with patched chromedriver $PATCHED_CHROMEDRIVER"
 
 if [[ $APPIUM_VERSION == 1.5.* ]]; then
   echo "Installing java dependency"
@@ -8,16 +8,21 @@ if [[ $APPIUM_VERSION == 1.5.* ]]; then
   apt-get -y install default-jdk
 fi
 
+appium_directory=/root/appium/appium/$APPIUM_VERSION
+if [ "$PATCHED_CHROMEDRIVER" = true ]; then
+  appium_directory=$appium_directory-patched-chromedriver
+fi
+
 npm install appium@$APPIUM_VERSION
 mkdir -p /root/appium/appium/
-mv /root/node_modules/appium /root/appium/appium/$APPIUM_VERSION
-mkdir /root/appium/appium/$APPIUM_VERSION/build/SafariLauncher
-cp --no-clobber /root/SafariLauncher.zip /root/appium/appium/$APPIUM_VERSION/build/SafariLauncher/SafariLauncher.zip
+mv /root/node_modules/appium $appium_directory
+mkdir $appium_directory/build/SafariLauncher
+cp --no-clobber /root/SafariLauncher.zip $appium_directory/build/SafariLauncher/SafariLauncher.zip
 rm /root/SafariLauncher.zip
 
 if [ "$APPIUM_VERSION" == '1.4.16' ]; then
   echo "replacing chromium driver"
-  cd /root/appium/appium/$APPIUM_VERSION
+  cd $appium_directory
   npm install appium-chromedriver@2.5.1
 
   # fix max buffer exceeded by increasing the buffer to 1MB
@@ -26,3 +31,15 @@ if [ "$APPIUM_VERSION" == '1.4.16' ]; then
 
   patch -p0 < /root/patches/0001-handover-custom-adb-port-to-chromedriver.patch;
 fi
+
+chromedriver_directory=$appium_directory/node_modules/appium-android-driver/node_modules/appium-chromedriver/chromedriver/linux
+
+if [ "$PATCHED_CHROMEDRIVER" = true ]; then
+  echo "Patching chromedriver"
+    
+  rm -r $chromedriver_directory
+  mv /root/patched-chromedriver-linux $chromedriver_directory
+else
+  rm -r /root/patched-chromedriver-linux
+fi
+
