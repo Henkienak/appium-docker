9c5fe1f0dded88338702b344cabc2b61ce32ef35
Malcolm Crum <crummynz@gmail.com>
2016-12-16 14:30:26 +0100

    Remove deprecated -f tag

--- build.sh
+++ build.sh
@@ -31,9 +31,9 @@ done
 docker build --build-arg APPIUM_VERSION=$appium_version --build-arg PATCHED_CHROMEDRIVER=$patched_chromedriver --build-arg CHROMEDRIVER_VERSION=$chromedriver_version -t testobject-appium:$tag .
 
 if [ "$release_images" = true ]; then
-    docker tag -f testobject-appium:$tag testobject/appium:$tag
+    docker tag testobject-appium:$tag testobject/appium:$tag
     docker push testobject/appium:$tag
 
-    docker tag -f testobject-appium:$tag quay.io/testobject/appium:$tag
+    docker tag testobject-appium:$tag quay.io/testobject/appium:$tag
     docker push quay.io/testobject/appium:$tag
 fi
